cdc4e2796cf2ed4bdd2e0f3d502c8c37c0090550
rygim <ry.gimmy@gmail.com>
2019-06-17 23:37:05 -0400

    update generator

--- generators/index.js
+++ generators/index.js
@@ -4,11 +4,7 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-method1() {
-    this.log('method 1 just ran');
-  }
   writing() {
-    this.log('copumg');
     this.fs.copy(
       this.templatePath(''),
       this.destinationPath('.')
--- generators/templates/scripts/run.sh
+++ generators/templates/scripts/run.sh
@@ -6,8 +6,6 @@ VIRTUALENV_DIR=$TOP_DIR/venv
 
 cd $TOP_DIR
 
-[ -d "$VIRTUALENV_DIR" ] || virtualenv $VIRTUALENV_DIR -p `which python3`
-
 source $VIRTUALENV_DIR/bin/activate
 
 python app.py
